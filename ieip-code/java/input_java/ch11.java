de5ef2252c36eba62742b6baa5313248e0475d31
HongJinHyeok <redmax45@naver.com>
2022-03-17 22:07:17 +0900

    11번 기출문제 (java)

---
+++ ieip-code/java/input_java/ch11.java
@@ -0,0 +1,12 @@
+
+public class ch11 {
+  public static void main(String[] args) {
+    int n = 17;
+    n += 1;
+    n -= 2;
+    n *= 3;
+    n /= 4;
+    n %= 5;
+    System.out.println(n);
+  }
+}
