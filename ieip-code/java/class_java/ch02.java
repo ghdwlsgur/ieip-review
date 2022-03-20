8a24d7f2e2aa3c07330910e13cea38ff613eb79b
HongJinHyeok <redmax45@naver.com>
2022-03-20 23:14:35 +0900

    2번 문제풀이 (java)

---
+++ ieip-code/java/class_java/ch02.java
@@ -0,0 +1,24 @@
+class A {
+  int a;
+
+  public A(int a) {
+    this.a = a;
+  }
+
+  void display() {
+    System.out.println("a=" + a);
+  }
+}
+
+class B extends A {
+  public B(int a) {
+    super(a);
+    super.display();
+  }
+}
+
+public class ch02 {
+  public static void main(String[] args) {
+    B obj = new B(10);
+  }
+}
