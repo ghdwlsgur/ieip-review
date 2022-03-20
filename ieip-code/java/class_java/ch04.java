012af4f69decb2e830991fc2437e008e79362b7e
HongJinHyeok <redmax45@naver.com>
2022-03-20 23:14:45 +0900

    4번 문제풀이 (java)

---
+++ ieip-code/java/class_java/ch04.java
@@ -0,0 +1,11 @@
+
+public class ch04 {
+  public static void main(String[] args) {
+    System.out.print(ch04.check(1));
+  }
+
+  static String check(int num) {
+    return (num >= 0) ? "positive" : "negative";
+  }
+
+}
