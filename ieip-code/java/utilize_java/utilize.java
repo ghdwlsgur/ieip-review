5be2e750b9e12240e5fd10724956b4a3046a60c8
HongJinHyeok <redmax45@naver.com>
2022-03-21 22:40:05 +0900

    자바 활용

---
+++ ieip-code/java/utilize_java/utilize.java
@@ -0,0 +1,31 @@
+abstract class Animal {
+  String a = " is animal";
+
+  abstract void look();
+
+  void show() {
+    System.out.println("Zoo");
+  }
+}
+
+class Chicken extends Animal {
+  Chicken() {
+    look();
+  }
+
+  void look() {
+    System.out.println("Chicken" + a);
+  }
+
+  void display() {
+    System.out.println("two wings");
+  }
+}
+
+public class utilize {
+  public static void main(String[] args) {
+    Animal a = new Chicken();
+    a.show();
+    a.look();
+  }
+}
