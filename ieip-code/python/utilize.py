1325d939624cd402ff6c21fb695fc52e394585c9
HongJinHyeok <redmax45@naver.com>
2022-03-22 21:39:35 +0900

    Python 활용

--- ieip-code/python/utilize.py
+++ ieip-code/python/utilize.py
@@ -1,5 +1,12 @@
-x = input("test: ")
+x = input("test: ")  # xyz321
 a = ['abc123', 'def456', 'ghi789']
-a.append(x)
-a.remove('def456')
+a.append(x)  # ['abc123', 'def456', 'ghi789', 'xyz321']
+a.remove('def456')  # ['abc123', 'ghi789', 'xyz321']
+# 'ghi789'
+# -6 -5 -4 -3 -2 -1
+# 789
+
+# 'xyz321'
+# -6 -5 -4 -3 -2 -1
+# xyz
 print(a[1][-3:], a[2][:-3], sep=',')
