e9dddf7cec18989aab50f2cfb4a6ddb0ac23ac1a
HongJinHyeok <redmax45@naver.com>
2022-03-24 23:25:40 +0900

    기출문제 23번

---
+++ ieip-code/java/final_java/ch23.java
@@ -0,0 +1,9 @@
+public class ch23 {
+  public static void main(String[] args) {
+    String str;
+    str = "Power overwhelming!";
+    System.out.printf("%8.4s\n", str); // 전체 8자리 중 오른쪽부터 4자리 ' Powe'
+    System.out.printf("%d\n", str.length());
+  }
+
+}
